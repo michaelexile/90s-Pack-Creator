22b55b0fcebe70e444f70fa6f67ee3d8e894dbf6
michaelexile <analogvoids@gmail.com>
2026-01-22 23:24:44 -0600

    prepping for DBD wiki sync update

---
+++ src/scripts/dbd-wiki-sync.ts
@@ -0,0 +1,31 @@
+const ADDON_RARITIES = [
+  "Common",
+  "Uncommon",
+  "Rare",
+  "Very_Rare",
+  "Visceral",
+  "Event",
+]; //Add-ons
+const ITEM_RARITIES = ["Common", "Uncommon", "Rare", "Very_Rare", "Event"]; // Items use Ultra Rare instead of Visceral usually
+const OFFERING_RARITIES = [
+  "Common",
+  "Uncommon",
+  "Rare",
+  "Very_Rare",
+  "Visceral",
+  "Event",
+];
+
+//To-Do
+//
+//
+// Create an async function that will sync with the wiki
+// `https://deadbydaylight.wiki.gg/api.php?${params}` using necessary params
+//
+//
+// if a response is received, save response to a const
+//
+// map each pages image name to their corresponding rarity
+//
+// save to a json file that corresponds to the icon type
+//
